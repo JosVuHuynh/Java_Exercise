5c8d5f594e0b794d4d228237922e58f544ed7d14
Vu Van Huynh <hvv@elcaNet.local>
2020-09-16 16:30:55 +0700

    new today

--- src/App.java
+++
@@ -1,9 +0,0 @@
-public class App {
-    public static void main(String[] args) {
-        String path= "C:\\Users\\hvv\\Desktop\\Code\\Design_Patten\\Exercises\\companies.csv";
-        FileFactory fileFactory = new FileFactory();
-        FileOperation file = fileFactory.getFile("CSV");
-        file.readFile(path);
-        System.out.println(file.readFile(path));
-    }
-}
--- src/FileFactory.java
+++
@@ -1,8 +0,0 @@
-public class FileFactory {
-    public FileOperation getFile(String fileType) {
-        if (fileType.equalsIgnoreCase("CSV")) {
-            return new CSVReader();
-        }
-    return null;
-    }
-}
